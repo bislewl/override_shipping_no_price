419e7c0522eb14c316739559817e6aa52849948e
Larry Bislew <bislewl@gmail.com>
2014-07-30 16:38:12 -0500

    fix defines issues

--- includes/classes/observers/class.override_shipping_no_price.php
+++ includes/classes/observers/class.override_shipping_no_price.php
@@ -16,29 +16,21 @@ class overrideShippingNoPriceObserver extends base {
 			$eligible_products = explode(',', $eligible_products_to_be_checked);
 
 			$products = $_SESSION['cart']->get_products();
-			$eligible_product_found = 0;
-                        
+			
 			
 				foreach ($products as $product) {
 					if (in_array((int)$product['id'], $eligible_products)) {
-						$eligible_product_found = 1;
-                                                
-						break;                                                                 
+						define('MODULE_SHIPPING_FREESHIPPER_TEXT_TITLE', CONFIGURATION_OVERRIDESHIPPING_NO_PRICE_TITLE);
+                                                define('MODULE_SHIPPING_FREESHIPPER_TEXT_DESCRIPTION', CONFIGURATION_OVERRIDESHIPPING_NO_PRICE_DESCRIPTION);
+                                                define('MODULE_SHIPPING_FREESHIPPER_TEXT_WAY', CONFIGURATION_OVERRIDESHIPPING_NO_PRICE_WAY);
+                                                break;                                                                 
 					}                    
                                         else{
                                             
                                         }
 				}
                                 
-				if ($eligible_product_found == 1) {
-					define('MODULE_SHIPPING_FREESHIPPER_TEXT_TITLE', CONFIGURATION_OVERRIDESHIPPING_NO_PRICE_TITLE);
-                                        define('MODULE_SHIPPING_FREESHIPPER_TEXT_DESCRIPTION', CONFIGURATION_OVERRIDESHIPPING_NO_PRICE_DESCRIPTION);
-                                        define('MODULE_SHIPPING_FREESHIPPER_TEXT_WAY', CONFIGURATION_OVERRIDESHIPPING_NO_PRICE_WAY);
-				} else {
-                                        define('MODULE_SHIPPING_FREESHIPPER_TEXT_TITLE', 'FREE SHIPPING!');
-                                        define('MODULE_SHIPPING_FREESHIPPER_TEXT_DESCRIPTION', 'FREE SHIPPING');
-                                        define('MODULE_SHIPPING_FREESHIPPER_TEXT_WAY', 'No Delivery Charge');
-				}
+                                        
 			
 		}
 	}
